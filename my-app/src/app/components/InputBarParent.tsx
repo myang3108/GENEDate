a201abdb64221e4682fd5e0421d129e6564a72ed
Michael Yang <myang4035@gmail.com>
2024-02-24 03:02:21 -0600

    night1

--- my-app/src/app/components/InputBarParent.tsx
+++ my-app/src/app/components/InputBarParent.tsx
@@ -23,7 +23,7 @@ const InputBarParent: React.FC = () => {
 
   return (
     <div>
-      <InputBar placeholder="Type something and press Enter" onUpdate={setInputValue} />
+      <InputBar placeholder="Keywords:" onUpdate={setInputValue} />
       <button onClick={handleButtonClick} className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Click me</button>
     </div>
   );
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -21,9 +21,9 @@ export default function Home() {
           title 
       */}
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <h1 className="mb-4 text-4xl font-extrabold text-gray-900 dark:text-white md:text-7xl lg:text-8xl">
+        <h1 className="mb-4 text-5xl font-extrabold text-gray-900 dark:text-white md:text-7xl lg:text-8xl">
           <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
-            (gened)ate
+            (gened)ate.
           </span>
         </h1>
       </div>
@@ -33,7 +33,6 @@ export default function Home() {
       */}
       <div>
         <main className="p-8">
-          <InputBar placeholder="keywords:" />
           <InputBarParent/>
         </main>
       </div>
@@ -42,23 +41,7 @@ export default function Home() {
       <AboutButton />
     </div>
 
-      <div className="mb-32 mx-auto lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <div className="grid-place-items-center">
-          <button
-            type="button"
-            className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-          >
-            Cyan to Blue
-          </button>
-          <Button
-            className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-            gradientDuoTone="purpleToBlue"
-          >
-            {" "}
-            Hello{" "}
-          </Button>
-        </div>
-      </div>
+      
 
       {/*
           background
