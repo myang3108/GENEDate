ce25db9955788e9a0908f599d5840ca1f9db1a60
Michael Yang <myang4035@gmail.com>
2024-02-24 02:01:50 -0600

    jerry cooking

--- my-app/src/app/components/InputBarParent.tsx
+++ my-app/src/app/components/InputBarParent.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import InputBar from "./InputBar";
+import axios from 'axios';
 
 const InputBarParent: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>("");
@@ -9,6 +10,13 @@ const InputBarParent: React.FC = () => {
   const handleButtonClick = () => {
     console.log("Button clicked!");
     console.log(inputValue);
+    axios.post('http://localhost:3000/calculate', { data: inputValue })
+    .then(response => {
+      console.log(response.data); // Handle the response from Flask
+    })
+    .catch(error => {
+      console.error(error); // Handle errors
+    });
     // Add your button click logic here
     // SWITCH TO THE RESULT PAGE
   };
