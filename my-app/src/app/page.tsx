fdb37cec1249096b28fecb8b4c945706ff61b508
Michael Yang <myang4035@gmail.com>
2024-02-25 03:34:27 -0600

    fixed format

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
           title 
       */}
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <h1 className="mt-0 text-5xl font-extrabold text-gray-900 dark:text-white md:text-7xl lg:text-8xl">
+        <h1 className="mt-0 mb-13 text-5xl font-extrabold text-gray-900 dark:text-white md:text-7xl lg:text-8xl">
           <span className="text-transparent bg-clip-text bg-gradient-to-r to-[#A890FE] from-[#EA8D8D]">
             (gened)ate.
           </span>
@@ -38,7 +38,7 @@ export default function Home() {
           input field
       */}
       <div>
-        <main className="top - 2 p-8">
+        <main className="mb-8 p-7">
           <InputBarParent />
         </main>
       </div>
