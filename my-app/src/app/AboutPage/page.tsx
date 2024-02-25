ec3b3370d359ca28bc255ed8926fff9f95b4ab06
Michael Yang <myang4035@gmail.com>
2024-02-25 03:17:11 -0600

    fixed about page

--- my-app/src/app/AboutPage/page.tsx
+++ my-app/src/app/AboutPage/page.tsx
@@ -1,12 +1,13 @@
 "use client";
-
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LinksPage from "../pages/page";
 import BackButton from "../components/BackButton";
-import lebron from "../images/lebron.png";
-import michael from "../images/michael_ss.jpg";
-import jerry from "../images/jerry.jpg";
+import aman from "../images/aman.jpeg";
+import michael from "../images/michael.jpeg";
+import jerry from "../images/jerry.jpeg";
+import vasu from "../images/vasu.jpeg";
+
 const aboutus: React.FC = () => {
   return (
     <main className="h-full overflow-hidden flex min-h-screen flex-col items-center justify-between p-24">
@@ -18,62 +19,64 @@ const aboutus: React.FC = () => {
       */}
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <h1 className="mb-4 text-5xl font-extrabold text-gray-900 dark:text-white md:text-7xl lg:text-8xl">
-          <span className="text-transparent bg-clip-text bg-gradient-to-r to-[#A9F1DF] from-[#FFBBBB]">
-            (gened)ate.
+          <span className="text-transparent bg-clip-text bg-gradient-to-r to-[#A890FE] from-[#EA8D8D]">
+            welcome to (gened)ate.
           </span>
         </h1>
       </div>
+
       <div>
+        <h1 className="mb-0 text-3xl font-extrabold text-gray-900 dark:text-white md:text-4xl lg:text-4xl">
+          <span className="text-transparent bg-clip-text bg-gradient-to-r to-[#A890FE] from-[#EA8D8D]">
+            meet the team!
+          </span>
+        </h1>
+
+
         <div className="fixed top-0 right-0 p-7">
           <BackButton />
         </div>
       </div>
       {/* 4 cards */}
       <div className="flex justify-around">
-        <div className="max-w-sm overflow-hidden bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <a href="#">
+        <div className="flex-none w-80 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-4">
+          <a href="https://www.linkedin.com/in/michaelyang3108">
             <img
-              className="rounded-t-lg w-full h-32 object-cover object-center"
+              className="w-full h-72 object-cover object-center rounded-t-lg"
               src={michael.src}
               alt=""
             />
           </a>
           <div className="p-5">
-            <a href="#">
+            <a href="https://www.linkedin.com/in/michaelyang3108">
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                 Michael Yang
               </h5>
             </a>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Floor general, shifty, deadly 3-pt shot.
-            </p>
           </div>
         </div>
 
-        <div className="max-w-sm overflow-hidden bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <a href="#">
+        <div className="flex-none w-80 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-4">
+          <a href="https://www.linkedin.com/in/aman-ravi-2405lhj/">
             <img
-              className="rounded-t-lg w-full h-32 object-cover object-center"
-              src={michael.src}
+              className="w-full h-72 object-cover object-center rounded-t-lg"
+              src={aman.src}
               alt=""
             />
           </a>
           <div className="p-5">
-            <a href="#">
+            <a href="https://www.linkedin.com/in/aman-ravi-2405lhj/">
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Michael Yang
+                Aman Ravishankar
               </h5>
             </a>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Floor general, shifty, deadly 3-pt shot.
-            </p>
           </div>
         </div>
 
-        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div className="flex-none w-80 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-4">
           <a href="https://www.linkedin.com/in/jerryluo-/">
             <img
-              className="rounded-t-lg w-full h-32 object-cover object-center"
+              className="w-full h-72 object-cover object-center rounded-t-lg"
               src={jerry.src}
               alt=""
             />
@@ -84,32 +87,27 @@ const aboutus: React.FC = () => {
                 Jerry Luo
               </h5>
             </a>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              The GOAT.
-            </p>
           </div>
         </div>
 
-        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <a href="https://www.linkedin.com/in/jerryluo-/">
+        <div className="flex-none w-80 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-4">
+          <a href="https://www.linkedin.com/in/vasunandan-dar/">
             <img
-              className="rounded-t-lg w-full h-32 object-cover object-center"
-              src={jerry.src}
+              className="w-full h-72 object-cover object-center rounded-t-lg"
+              src={vasu.src}
               alt=""
             />
           </a>
           <div className="p-5">
-            <a href="https://www.linkedin.com/in/jerryluo-/">
+            <a href="https://www.linkedin.com/in/vasunandan-dar/">
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Jerry Luo
+                Vasu Dar
               </h5>
             </a>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              The GOAT.
-            </p>
           </div>
         </div>
       </div>
+
       <BrowserRouter>
         <Routes>
           <Route path="/links" Component={LinksPage} />
--- my-app/src/app/components/BackButton.tsx
+++ my-app/src/app/components/BackButton.tsx
@@ -3,12 +3,11 @@ const BackButton: React.FC = () => {
   return (
     <a
       href="../"
-      className="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md"
-    >
-      <span className="w-full h-full bg-gradient-to-br to-[#A9F1DF] from-[#FFBBBB] group-hover:from-[#FFBBBB] group-hover:to-[#A9F1DF] absolute"></span>
-      <span className="relative px-6 py-3 transition-all ease-out bg-[#1a1a1a] rounded-md group-hover:bg-opacity-0 duration-400">
-        <span className="relative text-white">back</span>
-      </span>
+      className="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md">
+      <span className="w-full h-full bg-gradient-to-br to-[#A890FE] from-[#EA8D8D] group-hover:from-[#EA8D8D] group-hover:to-[#A890FE] absolute"></span>
+       <span className="relative px-6 py-3 transition-all ease-out bg-[#1a1a1a] rounded-md group-hover:bg-opacity-0 duration-400">
+         <span className="relative text-white">back</span>
+       </span>
     </a>
   );
 };
