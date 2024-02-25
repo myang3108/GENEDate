de3f9581e4859c67dac660c14520d9f4132a21da
jerryluo11 <89274680+jerryluo11@users.noreply.github.com>
2024-02-25 01:08:03 -0600

    got date from the backend

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,12 @@
 from flask import Flask
 import datetime
- 
+from flask_cors import CORS
+
 x = datetime.datetime.now()
  
 # Initializing flask app
 app = Flask(__name__)
- 
+CORS(app)
  
 # Route for seeing a data
 @app.route('/pages')
@@ -13,9 +14,9 @@ def get_time():
  
     # Returning an api for showing in  reactjs
     return {
-        "Date":x, 
+        "month":x, 
         } 
      
 # Running app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port =8080)
