056520ac37b58a8d72bfc84bf8f1e932049ba376
jerryluo11 <89274680+jerryluo11@users.noreply.github.com>
2024-02-25 01:16:29 -0600

    installed venv

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,15 @@
 from flask import Flask
 import datetime
 from flask_cors import CORS
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet as wn
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem import SnowballStemmer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+from rake_nltk import Rake
 
 x = datetime.datetime.now()
  
@@ -10,13 +19,14 @@ CORS(app)
  
 # Route for seeing a data
 @app.route('/pages')
-def get_time():
- 
+def get_class():
+    
     # Returning an api for showing in  reactjs
     return {
         "month":x, 
         } 
-     
+
+
 # Running app
 if __name__ == '__main__':
     app.run(debug=True, port =8080)
